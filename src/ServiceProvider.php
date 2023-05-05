638f1b2b40581f279073c5fd00b8e2f3d65f1efa
Thomas Kuschan <mail@thomaskuschan.de>
2023-05-05 16:52:23 +0200

    Initial data

---
+++ src/ServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Devdot\DeployArtisan;
+
+use Illuminate\Foundation\Console\AboutCommand;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        AboutCommand::add('devdot/deploy-artisan', fn () => [
+            'available' => true,
+        ]);
+
+        // install all our commands
+        $this->commands([
+        ]);
+    }
+}
