193fdd1ff4d95eb2a30273a2d51f58f6b5915859
Thomas Kuschan <mail@thomaskuschan.de>
2023-05-08 12:49:06 +0200

    Update default config file

--- src/config/deployment.php
+++ src/config/deployment.php
@@ -1,12 +1,30 @@
 <?php
 
-use Devdot\DeployArtisan\DeployCommands\ShellCommand;
+use Devdot\DeployArtisan\DeployCommands\ArtisanCommand;
+use Devdot\DeployArtisan\DeployCommands\CleanupCommand;
+use Devdot\DeployArtisan\DeployCommands\UnzipTransferFileCommand;
+use Devdot\DeployArtisan\DeployCommands\ZipTransferFileCommand;
 
 return [
+    /**
+     * Paths to the files and directories, that should be packed into the transfer file
+     * Use Laravel path helpers (base_path, public_path etc)
+     */
+    'transfer_files' => [
+        public_path('build'),
+        public_path('js'),
+        public_path('css'),
+    ],
+
     /**
      * Name of the file that is sent from client to server
      */
     'transfer_file_name' => 'deployment.zip',
+
+    /**
+     * Verify the git status when deploying from client to server
+     */
+    'verify_git' => true,
 
     /**
      * Run cleanup routine after push/pull
@@ -17,18 +35,24 @@ return [
      * Commands to run on the client
      */
     'client' => [
+        'git push',
         'vite build',
-        'git checkout public/build/assets/.htaccess',
-        'rm -f deployment.zip',
-        'zip -r deployment.zip public/build',
-        'rm -f deployment.zip',
-        ShellCommand::class,
+        [ZipTransferFileCommand::class, 'use_shell' => false],
     ],
 
     /**
      * Commands to run on the server
      */
     'server' => [
-
+        [ArtisanCommand::class, 'cmd' => 'down'],
+        'git pull',
+        [UnzipTransferFileCommand::class, 'use_shell' => false],
+        'composer install --optimize-autoloader --no-dev',
+        [ArtisanCommand::class, 'cmd' => 'migrate --step --force'],
+        [ArtisanCommand::class, 'cmd' => 'view:cache'],
+        [ArtisanCommand::class, 'cmd' => 'config:cache'],
+        [ArtisanCommand::class, 'cmd' => 'route:cache'],
+        [ArtisanCommand::class, 'cmd' => 'event:cache'],
+        [ArtisanCommand::class, 'cmd' => 'up'],
     ],
 ];
